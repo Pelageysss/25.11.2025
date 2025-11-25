3067537399ded83df2b46ff3f9f334662633abf6
Pelagey <polina221007@icloud.com>
2025-11-25 09:35:34 +0300

    №16_2

---
+++ №16_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int lengthOfLIS(vector<int>& nums) {
+    if (nums.empty()) return 0;
+    vector<int> dp(nums.size(), 1);
+    int max_len = 1;
+    for (int i = 1; i < nums.size(); i++) {
+        for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        max_len = max(max_len, dp[i]);
+    }
+    return max_len;
+}
+int main() {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << lengthOfLIS(nums) << endl;
+    return 0;
+}
